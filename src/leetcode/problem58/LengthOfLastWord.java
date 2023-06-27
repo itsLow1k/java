cbc50abd096a755a51d322095042e8f5215f8549
itsLow1k <thisislow1996@gmail.com>
2023-06-27 23:21:09 +0300

    test

--- src/leetcode/problem58/LengthOfLastWord.java
+++ src/leetcode/problem58/LengthOfLastWord.java
@@ -3,10 +3,10 @@ package leetcode.problem58;
 public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
         int counter = 0;
-        for (int i = s.length(); i != 0; i--){
-            if (s.charAt(i-1) != ' '){
+        for (int i = s.length(); i != 0; i--) {
+            if (s.charAt(i - 1) != ' ') {
                 counter += 1;
-            } else if (s.charAt(i-1) == ' ' && counter >= 1){
+            } else if (counter >= 1) {
                 break;
             }
         }
