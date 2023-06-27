7a72f69b3f1980f1d5414861cb723d5f69134516
itsLow1k <thisislow1996@gmail.com>
2023-06-27 21:31:25 +0300

    nn

--- src/leetcode/problem27/Solution.java
+++ src/leetcode/problem27/Solution.java
@@ -4,28 +4,21 @@ import java.util.ArrayList;
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int counter = 0;
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int num : nums) {
             if (num != val){
                 list.add(num);
-                counter += 1;
             }
 
         }
-        for (int i = 0; i < nums.length; i++){
-            if (i < list.size()){
-                nums[i] = list.get(i);
-            } else {
-                nums[i] = 0;
-            }
-            System.out.println(nums[i]);
+        for (int i = 0; i < list.size(); i++){
+            nums[i] = list.get(i);
         }
-        return counter;
+        return list.size();
     }
 
     public static void main(String[] args) {
         Solution s1 = new Solution();
-        s1.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        s1.removeElement(new int[]{4,5}, 4);
     }
 }
