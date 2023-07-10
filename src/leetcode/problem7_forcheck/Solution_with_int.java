318b22657d08182a96bcec3f3e708d5c2f3ff0e2
itsLow1k <thisislow1996@gmail.com>
2023-07-10 20:29:22 +0300

    test

--- src/leetcode/problem7_forcheck/Solution_with_int.java
+++ src/leetcode/problem7_forcheck/Solution_with_int.java
@@ -1,6 +1,6 @@
 package leetcode.problem7_forcheck;
 
-public class Solution2 {
+public class Solution_with_int {
     public int reverse(int x) {
         int reversedNum;
         StringBuilder numFromIntVar = new StringBuilder(String.valueOf(x));
@@ -20,7 +20,7 @@ public class Solution2 {
     }
 
     public static void main(String[] args) {
-        Solution2 s = new Solution2();
+        Solution_with_int s = new Solution_with_int();
         s.reverse(-20);
     }
 }
