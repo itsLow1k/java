521394a6d7a63ddb188dd5cbda7a57a89c552594
itsLow1k <thisislow1996@gmail.com>
2023-07-25 14:40:30 +0300

    test

---
+++ src/leetcode/problem3/SolutionSB.java
@@ -0,0 +1,30 @@
+package leetcode.problem3;
+
+public class SolutionSB {
+    public int lengthOfLongestSubstring(String string) {
+        int maxLength = 0;
+        StringBuilder startString = new StringBuilder(string);
+        StringBuilder substringWithoutRepeating = new StringBuilder();
+        for (int i = 0; i < startString.length(); i++) {
+            if (!substringWithoutRepeating.toString().contains(String.valueOf(startString.charAt(i)))) {
+                substringWithoutRepeating.append(startString.charAt(i));
+            } else {
+                startString.delete(0, startString.indexOf(String.valueOf(startString.charAt(i)))+1);
+                substringWithoutRepeating.setLength(0);
+                i=0;
+                substringWithoutRepeating.append(startString.charAt(i));
+            }
+            if (maxLength < substringWithoutRepeating.length()){
+                maxLength = substringWithoutRepeating.length();
+            }
+            System.out.println(maxLength);
+            System.out.println(substringWithoutRepeating);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        SolutionSB solution = new SolutionSB();
+        solution.lengthOfLongestSubstring("abcabcbb");
+    }
+}
