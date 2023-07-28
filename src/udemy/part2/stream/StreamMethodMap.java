caf0ebddbdf5b83ca6e49b665136ddb23fe21af8
itsLow1k <thisislow1996@gmail.com>
2023-07-29 01:37:42 +0300

    test

---
+++ src/udemy/part2/stream/StreamMethodMap.java
@@ -0,0 +1,28 @@
+package udemy.part2.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMethodMap {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("hello");
+        list1.add("how are u");
+        list1.add("nice");
+        list1.add("bye");
+
+        list1 = list1.stream().map(element -> String.valueOf(element.length())).toList();
+        System.out.println(list1);
+
+        int[] array = {3,6,2,5,9,16,21};
+        array = Arrays.stream(array).map(element -> {
+            if (element % 3 == 0){
+                element /= 3;
+            }
+            return element;
+        }).toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
